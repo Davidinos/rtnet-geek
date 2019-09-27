894e38fff09c877c6c861a7a49c8c12e02cdffab
Laurentiu-Cristian Duca <laurentiu.duca@gmail.com>
2019-09-27 16:12:31 +0300

    1st

---
+++ beaglebone-black/rtnet-test/frag-ip.c
@@ -0,0 +1,262 @@
+/***
+ *
+ *  examples/xenomai/frag-ip.c
+ *
+ *  sends fragmented IP packets to another frag-ip instance - Xenomai version
+ *
+ *  Copyright (C) 2003-2005 Jan Kiszka <jan.kiszka@web.de>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <task.h>
+#include <rtnet/rtnet.h>
+
+#include <trank/rtdm/rtdm.h>
+//#include <rtnet/rtnet_config.h>   /* required for rt_task_wait_period() changes */
+
+char *dest_ip_s = "127.0.0.1";
+unsigned int size = 65505;
+unsigned int add_rtskbs = 75;
+
+#define CYCLE       1000*1000*1000   /* 1 s */
+RT_TASK rt_xmit_task;
+RT_TASK rt_recv_task;
+
+#define PORT        37000
+
+struct sockaddr_in dest_addr;
+
+int sock;
+
+char buffer_out[64*1024];
+char buffer_in[64*1024];
+
+
+void xmit_msg(void *arg)
+{
+    int ret;
+    struct msghdr msg;
+    struct iovec iov[2];
+    unsigned short msgsize = size;
+
+
+    rt_task_set_periodic(NULL, TM_NOW, CYCLE);
+    while (1) {
+        iov[0].iov_base = &msgsize;
+        iov[0].iov_len  = sizeof(msgsize);
+        iov[1].iov_base = buffer_out;
+        iov[1].iov_len  = size;
+
+        memset(&msg, 0, sizeof(msg));
+        msg.msg_name    = &dest_addr;
+        msg.msg_namelen = sizeof(dest_addr);
+        msg.msg_iov     = iov;
+        msg.msg_iovlen  = 2;
+
+        printf("Sending message of %d+2 bytes\n", size);
+        ret = rt_dev_sendmsg(sock, &msg, 0);
+        if (ret == -EBADF)
+            return;
+        else if (ret != (int)(sizeof(msgsize) + size)) {
+	    perror("rt_dev_sendmsg");	
+            printf(" rt_dev_sendmsg() = %d! EINVAL=%d\n", ret, EINVAL);
+	}
+
+#ifdef CONFIG_XENO_2_0x /* imported via rtnet_config.h */
+        rt_task_wait_period(); /* old signature */
+#else /* Xenomai 2.1 and later */
+        rt_task_wait_period(NULL);
+#endif /* CONFIG_XENO_2_0x */
+    }
+}
+
+
+
+void recv_msg(void *arg)
+{
+    int ret;
+    struct msghdr msg;
+    struct iovec iov[2];
+    unsigned short msgsize = size;
+    struct sockaddr_in addr;
+
+
+    while (1) {
+        iov[0].iov_base = &msgsize;
+        iov[0].iov_len  = sizeof(msgsize);
+        iov[1].iov_base = buffer_in;
+        iov[1].iov_len  = size;
+
+        memset(&msg, 0, sizeof(msg));
+        msg.msg_name    = &addr;
+        msg.msg_namelen = sizeof(addr);
+        msg.msg_iov     = iov;
+        msg.msg_iovlen  = 2;
+
+        ret = rt_dev_recvmsg(sock, &msg, 0);
+        if (ret <= 0) {
+            perror("rt_dev_recvmsg:");
+	    printf(" rt_dev_recvmsg() = %d\n", ret);
+            return;
+        } else {
+            unsigned long ip = ntohl(addr.sin_addr.s_addr);
+
+            printf("received packet from %lu.%lu.%lu.%lu, length: %zd+2, "
+                "encoded length: %d,\n flags: %X, content %s\n", ip >> 24,
+                (ip >> 16) & 0xFF, (ip >> 8) & 0xFF, ip & 0xFF,
+                ret-sizeof(msgsize), msgsize, msg.msg_flags,
+                (memcmp(buffer_in, buffer_out, ret-sizeof(msgsize)) == 0) ?
+                    "ok" : "corrupted");
+        }
+    }
+}
+
+
+
+void catch_signal(int sig)
+{
+}
+
+
+int main(int argc, char *argv[])
+{
+    int ret;
+    unsigned int i;
+    struct sockaddr_in local_addr;
+    struct in_addr dest_ip;
+
+
+    while (1) {
+        switch (getopt(argc, argv, "d:s:")) {
+            case 'd':
+                dest_ip_s = optarg;
+                break;
+
+            case 's':
+                size = atoi(optarg);
+                break;
+
+            case -1:
+                goto end_of_opt;
+
+            default:
+                printf("usage: %s [-d <dest_ip>] [-s <size>]\n", argv[0]);
+                return 0;
+        }
+    }
+ end_of_opt:
+
+    inet_aton(dest_ip_s, &dest_ip);
+
+    if (size > 65505)
+        size = 65505;
+
+    signal(SIGTERM, catch_signal);
+    signal(SIGINT, catch_signal);
+    signal(SIGHUP, catch_signal);
+    mlockall(MCL_CURRENT|MCL_FUTURE);
+
+    printf("destination ip address %s=%08x\n", dest_ip_s, dest_ip.s_addr);
+    printf("size %d\n", size);
+
+    /* fill output buffer with test pattern */
+    for (i = 0; i < sizeof(buffer_out); i++)
+        buffer_out[i] = i & 0xFF;
+
+    /* create rt-socket */
+    sock = rt_dev_socket(AF_INET,SOCK_DGRAM,0);
+    if (sock < 0) {
+        printf(" rt_dev_socket() = %d!\n", sock);
+        return sock;
+    }
+
+    /* extend the socket pool */
+    ret = rt_dev_ioctl(sock, RTNET_RTIOC_EXTPOOL, &add_rtskbs);
+    if (ret != (int)add_rtskbs) {
+        printf(" rt_dev_ioctl(RT_IOC_SO_EXTPOOL) = %d\n", ret);
+        rt_dev_close(sock);
+        return -1;
+    }
+
+    /* bind the rt-socket to a port */
+    memset(&local_addr, 0, sizeof(struct sockaddr_in));
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port = htons(PORT);
+    local_addr.sin_addr.s_addr = INADDR_ANY;
+    ret = rt_dev_bind(sock, (struct sockaddr *)&local_addr,
+                      sizeof(struct sockaddr_in));
+    if (ret < 0) {
+        printf(" rt_dev_bind() = %d!\n", ret);
+        rt_dev_close(sock);
+        return ret;
+    }
+
+    /* set destination address */
+    memset(&dest_addr, 0, sizeof(struct sockaddr_in));
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT);
+    dest_addr.sin_addr = dest_ip;
+
+    /* You may have to start the system timer manually
+     * on older Xenomai versions (2.0.x):
+     * rt_timer_start(TM_ONESHOT); */
+
+    ret = rt_task_create(&rt_recv_task, "Receiver", 0, 10, 0);
+    if (ret != 0) {
+        printf(" rt_task_create(recv) = %d!\n", ret);
+        rt_dev_close(sock);
+        return ret;
+    }
+
+    ret = rt_task_create(&rt_xmit_task, "Sender", 0, 9, 0);
+    if (ret != 0) {
+        printf(" rt_task_create(xmit) = %d!\n", ret);
+        rt_dev_close(sock);
+        rt_task_delete(&rt_recv_task);
+        return ret;
+    }
+
+    rt_task_start(&rt_recv_task, recv_msg, NULL);
+    rt_task_start(&rt_xmit_task, xmit_msg, NULL);
+
+    pause();
+
+    /* In case you started it in this program, see comment above.
+     * rt_timer_stop(); */
+
+    /* Note: The following loop and the strict ordering "close before task
+     *       termination" is no longer required since Xenomai 2.4. */
+    while (rt_dev_close(sock) == -EAGAIN) {
+        printf("frag-ip: Socket busy - waiting...\n");
+        sleep(1);
+    }
+
+    rt_task_delete(&rt_xmit_task);
+    rt_task_delete(&rt_recv_task);
+
+    return 0;
+}
---
+++ beaglebone-black/rtnet-test/rtt-sender.c
@@ -0,0 +1,359 @@
+/***
+ *
+ *  examples/xenomai/posix/rtt-requester.c
+ *
+ *  Round-Trip Time Requester - sends packet, receives echo, evaluates
+ *                              and displays per-station round-trip times
+ *
+ *  Based on Ulrich Marx's module, adopted to RTmac and later ported over
+ *  user space POSIX.
+ *
+ *  Copyright (C) 2002 Ulrich Marx <marx@kammer.uni-hannover.de>
+ *                2002 Marc Kleine-Budde <kleine-budde@gmx.de>
+ *                2006 Jan Kiszka <jan.kiszka@web.de>
+ *
+ *  RTnet - real-time networking example
+ *  RTmac - real-time media access control example
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#include <errno.h>
+#include <mqueue.h>
+#include <signal.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/mman.h>
+#include <arpa/inet.h>
+#include <limits.h>
+
+#include <rtnet/rtnet.h>
+
+char *dest_ip_s = "127.0.0.1";
+char *local_ip_s = "";
+unsigned int cycle = 50000; /* 50 ms */
+
+pthread_t xmit_thread;
+pthread_t recv_thread;
+
+#define RCV_PORT                35999
+#define XMT_PORT                36000
+
+#define DEFAULT_ADD_BUFFERS     30
+
+struct sockaddr_in dest_addr;
+
+int sock;
+mqd_t mq;
+
+#define BUFSIZE 1500
+union {
+    char            data[BUFSIZE];
+    struct timespec tx_date;
+} packet;
+
+struct station_stats {
+    struct in_addr  addr;
+    long long       last, min, max;
+    unsigned long   count;
+};
+
+struct packet_stats {
+    struct in_addr  addr;
+    long long       rtt;
+};
+
+#define MAX_STATIONS 100
+static struct station_stats station[MAX_STATIONS];
+
+
+static struct station_stats *lookup_stats(struct in_addr addr)
+{
+    int i;
+
+    for (i = 0; i < MAX_STATIONS; i++) {
+        if (station[i].addr.s_addr == addr.s_addr)
+            break;
+        if (station[i].addr.s_addr == 0) {
+            station[i].addr = addr;
+            station[i].min  = LONG_MAX;
+            station[i].max  = LONG_MIN;
+            break;
+        }
+    }
+    if (i == MAX_STATIONS)
+        return NULL;
+    return &station[i];
+}
+
+
+void *transmitter(void *arg)
+{
+    struct sched_param  param = { .sched_priority = 80 };
+    struct timespec     next_period;
+    struct timespec     tx_date;
+
+
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
+
+    clock_gettime(CLOCK_MONOTONIC, &next_period);
+
+    while(1) {
+        next_period.tv_nsec += cycle * 1000;
+        if (next_period.tv_nsec >= 1000000000) {
+            next_period.tv_nsec = 0;
+            next_period.tv_sec++;
+        }
+
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next_period, NULL);
+
+        clock_gettime(CLOCK_MONOTONIC, &tx_date);
+
+        /* transmit the request packet containing the local time */
+        if (sendto(sock, &tx_date, sizeof(tx_date), 0,
+                   (struct sockaddr *)&dest_addr,
+                   sizeof(struct sockaddr_in)) < 0) {
+            if (errno == EBADF)
+                printf("terminating transmitter thread\n");
+            else
+                perror("sendto failed");
+            return NULL;
+        }
+    }
+}
+
+
+void *receiver(void *arg)
+{
+    struct sched_param  param = { .sched_priority = 82 };
+    struct msghdr       msg;
+    struct iovec        iov;
+    struct sockaddr_in  addr;
+    struct timespec     rx_date;
+    struct packet_stats stats;
+    int                 ret;
+
+
+    msg.msg_name       = &addr;
+    msg.msg_namelen    = sizeof(addr);
+    msg.msg_iov        = &iov;
+    msg.msg_iovlen     = 1;
+    msg.msg_control    = NULL;
+    msg.msg_controllen = 0;
+
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
+
+    while (1) {
+        iov.iov_base = &packet;
+        iov.iov_len  = sizeof(packet);
+
+        ret = recvmsg(sock, &msg, 0);
+        if (ret <= 0) {
+            printf("terminating receiver thread\n");
+            return NULL;
+        }
+
+        clock_gettime(CLOCK_MONOTONIC, &rx_date);
+        stats.rtt = rx_date.tv_sec * 1000000000LL + rx_date.tv_nsec;
+        stats.rtt -= packet.tx_date.tv_sec * 1000000000LL +
+            packet.tx_date.tv_nsec;
+        stats.addr = addr.sin_addr;
+
+        mq_send(mq, (char *)&stats, sizeof(stats), 0);
+    }
+}
+
+
+void catch_signal(int sig)
+{
+    mq_close(mq);
+}
+
+
+int main(int argc, char *argv[])
+{
+    struct sched_param param = { .sched_priority = 1 };
+    struct sockaddr_in local_addr;
+    int add_rtskbs = DEFAULT_ADD_BUFFERS;
+    pthread_attr_t thattr;
+    char mqname[32];
+    struct mq_attr mqattr;
+    int stations = 0;
+    int ret;
+
+
+    while (1) {
+        switch (getopt(argc, argv, "d:l:c:b:")) {
+            case 'd':
+                dest_ip_s = optarg;
+                break;
+
+            case 'l':
+                local_ip_s = optarg;
+                break;
+
+            case 'c':
+                cycle = atoi(optarg);
+                break;
+
+            case 'b':
+                add_rtskbs = atoi(optarg);
+
+            case -1:
+                goto end_of_opt;
+
+            default:
+                printf("usage: %s [-d <dest_ip>] [-l <local_ip>] "
+                       "[-c <cycle_microsecs>] [-b <add_buffers>]\n",
+                       argv[0]);
+                return 0;
+        }
+    }
+ end_of_opt:
+
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port   = htons(XMT_PORT);
+    if (dest_ip_s[0])
+        inet_aton(dest_ip_s, &dest_addr.sin_addr);
+    else
+        dest_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (local_ip_s[0])
+        inet_aton(local_ip_s, &local_addr.sin_addr);
+    else
+        local_addr.sin_addr.s_addr = INADDR_ANY;
+
+    signal(SIGTERM, catch_signal);
+    signal(SIGINT, catch_signal);
+    signal(SIGHUP, catch_signal);
+    mlockall(MCL_CURRENT|MCL_FUTURE);
+
+    printf("destination ip address: %s = %08x\n",
+           dest_ip_s[0] ? dest_ip_s : "SENDER", dest_addr.sin_addr.s_addr);
+    printf("local ip address: %s = %08x\n",
+           local_ip_s[0] ? local_ip_s : "INADDR_ANY", local_addr.sin_addr.s_addr);
+    printf("cycle: %d us\n", cycle);
+
+    /* create rt-socket */
+    if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        perror("socket cannot be created");
+        return 1;
+    }
+
+    /* bind the rt-socket to local_addr */
+    local_addr.sin_family = AF_INET;
+    local_addr.sin_port   = htons(RCV_PORT);
+    if (bind(sock, (struct sockaddr *)&local_addr, sizeof(local_addr)) < 0) {
+        perror("cannot bind to local ip/port");
+        close(sock);
+        return 1;
+    }
+
+    /* extend the socket pool */
+    ret = ioctl(sock, RTNET_RTIOC_EXTPOOL, &add_rtskbs);
+    if (ret != add_rtskbs)
+        perror("WARNING: ioctl(RTNET_RTIOC_EXTPOOL)");
+
+    /* create statistics message queue */
+    snprintf(mqname, sizeof(mqname), "/rtt-sender-%d", getpid());
+    mqattr.mq_flags   = 0;
+    mqattr.mq_maxmsg  = 100;
+    mqattr.mq_msgsize = sizeof(struct packet_stats);
+    mq = mq_open(mqname, O_RDWR | O_CREAT | O_EXCL, 0600, &mqattr);
+    if (mq == (mqd_t)-1) {
+        perror("opening mqueue failed");
+        close(sock);
+        return 1;
+    }
+
+    /* create transmitter rt-thread */
+    pthread_attr_init(&thattr);
+    pthread_attr_setdetachstate(&thattr, PTHREAD_CREATE_JOINABLE);
+    pthread_attr_setstacksize(&thattr, PTHREAD_STACK_MIN);
+    ret = pthread_create(&recv_thread, &thattr, &receiver, NULL);
+    if (ret) {
+        errno = ret; perror("pthread_create(receiver) failed");
+        close(sock);
+        mq_close(mq);
+        return 1;
+    }
+
+    /* create receiver rt-thread */
+    ret = pthread_create(&xmit_thread, &thattr, &transmitter, NULL);
+    if (ret) {
+        errno = ret; perror("pthread_create(transmitter) failed");
+        close(sock);
+        mq_close(mq);
+        pthread_kill(recv_thread, SIGHUP);
+        pthread_join(recv_thread, NULL);
+        return 1;
+    }
+
+    pthread_setschedparam(pthread_self(), SCHED_FIFO, &param);
+
+    while (1) {
+        struct packet_stats pack;
+        struct station_stats *pstat;
+        int nr;
+
+        ret = mq_receive(mq, (char *)&pack, sizeof(pack), NULL);
+        if (ret < (int)sizeof(pack))
+            break;
+
+        pstat = lookup_stats(pack.addr);
+        if (!pstat)
+            continue;
+
+        pstat->last = pack.rtt;
+        if (pstat->last < pstat->min)
+            pstat->min = pstat->last;
+        if (pstat->last > pstat->max)
+            pstat->max = pstat->last;
+        pstat->count++;
+
+        nr = pstat - &station[0];
+        if (nr >= stations) {
+            stations = nr+1;
+            printf("\n");
+        }
+
+        printf("\033[%dA%s\t%9.3f us, min=%9.3f us, max=%9.3f us, count=%ld\n",
+               stations-nr, inet_ntoa(pack.addr), (float)pstat->last/1000,
+               (float)pstat->min/1000, (float)pstat->max/1000, pstat->count);
+        for (nr = stations-nr-1; nr > 0; nr --)
+            printf("\n");
+    }
+
+    /* This call also leaves primary mode, required for socket cleanup. */
+    printf("shutting down\n");
+
+    /* Note: The following loop is no longer required since Xenomai 2.4,
+     *       plain close works as well. */
+    while ((close(sock) < 0) && (errno == EAGAIN)) {
+        printf("socket busy - waiting...\n");
+        sleep(1);
+    }
+
+    pthread_join(xmit_thread, NULL);
+    pthread_kill(recv_thread, SIGHUP);
+    pthread_join(recv_thread, NULL);
+
+    return 0;
+}
